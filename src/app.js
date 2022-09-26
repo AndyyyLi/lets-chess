f747e496445ff03b6f924e2c5f95077a1fa496ba
AndyyyLi <o4k0h@ugrad.cs.ubc.ca>
2022-09-26 09:18:48 -0700

    First commit of original files

---
+++ src/app.js
@@ -0,0 +1,119 @@
+import "./style.scss";
+
+import ScreenManagerInstance from "./screenManager";
+import RecordingManagerInstance from "./recordingManager";
+import PersistentDataManagerInstance from "./persistentDataManager";
+
+import SplashScreen from "./screens/splashScreen";
+import MenuScreen from "./screens/menuScreen";
+import GameplayScreen from "./screens/gameplayScreen";
+import RecordingScreen from "./screens/recordingScreen";
+import ReviewScreen from "./screens/reviewScreen";
+
+import { PreloadList, PreloadListLoader } from "./libs/Preloader";
+import { isCreatorMode } from "./util";
+import { ASSETS } from "./const";
+
+export default class App {
+    constructor() {    
+        this.runtime = o3h.Instance;
+        this.runtime.adjustViewport("device-width", "device-height");
+
+        this.assetManager = this.runtime.getAssetManager();
+        this.inputManager = this.runtime.getInputManager();
+
+        this.systemSettingsService = this.runtime.getSystemSettingsService();
+        this.analyticService = this.runtime.getAnalyticService();
+
+        this.splashScreen = new SplashScreen(this);
+        this.menuScreen = new MenuScreen(this);
+        this.gameplayScreen = new GameplayScreen(this);
+        this.recordingScreen = new RecordingScreen(this);
+        this.reviewScreen = new ReviewScreen(this);
+
+        this.message = ""; // Variable used to store the creator's message to the audience
+
+        this.init();
+    }
+
+    async init() {
+        const managerPreloader = new PreloadList();
+
+        managerPreloader.addLoad(() => RecordingManagerInstance.init(this.runtime.getNativeUIManager(), this.runtime.getControlManager(),
+        () => {
+            // Hide UI when recording starts
+        },
+        () => {
+            // Show the review screen when recording stops
+            this.showReview();
+        }));
+        managerPreloader.addLoad(() => PersistentDataManagerInstance.init());
+
+        await managerPreloader.loadAll();
+
+        const listPreloader = new PreloadListLoader();
+        listPreloader.addPreloadList(this.splashScreen.getPreloadList());
+        listPreloader.addPreloadList(this.menuScreen.getPreloadList());
+        listPreloader.addPreloadList(this.gameplayScreen.getPreloadList());
+        listPreloader.addPreloadList(this.recordingScreen.getPreloadList());
+        listPreloader.addPreloadList(this.reviewScreen.getPreloadList());
+    
+        listPreloader.loadAll();
+
+        await ScreenManagerInstance.showScreen(this.splashScreen);
+    
+        this.runtime.ready(() => {
+            // Start splash screen animation, music, etc.
+
+            // Show the title element to start its CSS animation
+            document.querySelector("#splashScreen .title").classList.remove("hidden");
+        });
+    }
+
+    async showMenu() {
+        await ScreenManagerInstance.showScreen(this.menuScreen);
+    }
+
+    async showGameplay() {
+        await ScreenManagerInstance.showScreen(this.gameplayScreen);
+    }
+
+    async showRecording() {
+        await ScreenManagerInstance.showScreen(this.recordingScreen);
+    }
+
+    async showReview() {
+        ScreenManagerInstance.showLoadingCover();
+        const fullScreenVideoAsset = RecordingManagerInstance.getFullScreenRecording();
+
+        await this.reviewScreen.getPreloadList().loadAll();
+
+        const videoPath = await fullScreenVideoAsset.getVideoPath();
+        await this.reviewScreen.reviewVideoComponent.prepareVideo(videoPath);
+
+        await ScreenManagerInstance.showScreen(this.reviewScreen);
+        ScreenManagerInstance.hideLoadingCover();
+    }
+
+    async exit() {
+        // Add the full-screen video recording as an output asset
+        this.assetManager.addToOutput(ASSETS.FULL_SCREEN_RECORDING, RecordingManagerInstance.getFullScreenRecording());
+
+        if (isCreatorMode()) {
+            // Add creator mode assets to output
+
+            const replayRecorder = await this.runtime.createReplayRecorder();
+            // Add the creator message as a replay data property
+            replayRecorder.addProperty("message", this.message);
+
+            // Get the replay data from the replay recorder and add it as an output asset
+            const replayData = await replayRecorder.getReplayData();
+            this.assetManager.addToOutput(ASSETS.REPLAY_DATA, replayData);
+        }
+
+        // End the module with a score of 0 (non-game module)
+        this.runtime.completeModule({
+            "score": 0
+        });
+    }
+}
